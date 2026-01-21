c86c4d8638aab22c7c2c9671ec3db7464cd7f9dc
Alveriana <alverianamohan@gmail.com>
2026-01-21 13:53:08 +0800

    Avee

---
+++ Backend/app/core/config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import Optional
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+settings = Settings()
